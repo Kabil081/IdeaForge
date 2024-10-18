1ff5f65eecbeb9b5b4c1acdddea2fa523b912737
Kabil081 <kabilram212@gmail.com>
2024-10-18 17:18:21 +0530

    feat:

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -8,5 +10,8 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       '@shadcn/ui': path.resolve(__dirname, 'node_modules/@shadcn/ui'),
     },
+  },
+  define: {
+    'process.env': process.env,
   },
 });
