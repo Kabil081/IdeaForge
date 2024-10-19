796d129b4168daa72192d2e859f5462d34f0b1e1
TSKailash <tskailash20@gmail.com>
2024-10-19 21:30:11 +0530

    feat:

--- front-end/src/components/Profile.jsx
+++ front-end/src/components/Profile.jsx
@@ -10,7 +10,7 @@ const Profile = () => {
         <input className="px-4 py-2 w-96 rounded-sm outline-none border-gray-600 border-[1px]" type="number" placeholder="Enter your savings"/>
         <input className="px-4 py-2 w-96 rounded-sm outline-none border-gray-600 border-[1px]" type="number" placeholder="Enter retirement age"/>
         <input className="px-4 py-2 w-96 rounded-sm outline-none border-gray-600 border-[1px]" type="number" placeholder="Enter retirement savings"/>
-        <input className="px-4 py-2 w-96 rounded-sm outline-none border-gray-600 border-[1px]" type="number" placeholder="Enter risk tolerance (1-10)"/>
+        <input className="px-4 py-2 w-96 rounded-sm outline-none border-gray-600 border-[1px]" type="number" placeholder="Enter risk tolerance (0-5)"/>
         <div className="flex justify-center w-full">
           <button className="bg-blue-500 font-semibold text-lg text-white px-[15px] py-[7px] rounded-[8px] w-[40%] hover:scale-[1.15] transition duration-300 active:scale-[1]">Submit</button>
         </div>
